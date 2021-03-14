2d00eb93781960d02bee623f42b4d1211cbbb226
Jess70 <jppatidarjay713@gmail.com>
2021-03-14 14:36:15 +0530

    Account, matters, task, activity, events

--- models/activity.js
+++ models/activity.js
@@ -1,25 +1,26 @@
 const mongoose = require("mongoose")
+const user = require("./user")
 const Schema = mongoose.Schema
 const SchemaOptions = {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
 }
 
 const Activity = new Schema({
-    type:String,
-    qty:String,
-    description:String,
-    rate:String,
-    date:{type:Date, default:Date.now},
-    user:{type:Schema.Types.ObjectId, ref: "User"},
-    matter:{type:Schema.Types.ObjectId, ref: "Matters"},
-    contact:[{contact:{type:Schema.Types.ObjectId, ref:"Contacts"}}],
-    invoiceStatus:String,
-    time:String,
-    billable:{type:Boolean, default: false},
-    amount:String,
-    userId:{type:Schema.Types.ObjectId, ref:"User"},
-    billed:{type:Boolean, default: false}
-
+    type: String,
+    qty: String,
+    description: String,
+    rate: String,
+    date: { type: Date, default: Date.now },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    matter: { type: Schema.Types.ObjectId, ref: "Matters" },
+    contact: [{ contact: { type: Schema.Types.ObjectId, ref: "Contacts" } }],
+    invoiceStatus: String,
+    time: String,
+    billable: { type: Boolean, default: false },
+    amount: String,
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    billed: { type: Boolean, default: false },
+    userName: String
 }, SchemaOptions)
 
 
--- models/calendar.js
+++ models/calendar.js
@@ -11,11 +11,12 @@ const Calendar = new Schema({
   type: String,
   location: String,
   matter: { type: Schema.Types.ObjectId, ref: "Matters" },
-  userId:{type:Schema.Types.ObjectId, ref:"User"},
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
   notification: { type: Boolean, default: false },
   email: { type: Boolean, default: false },
   timeForReminder: String,
   description: String,
+  userName: String
 });
 
 module.exports = mongoose.model("Calendar", Calendar);
